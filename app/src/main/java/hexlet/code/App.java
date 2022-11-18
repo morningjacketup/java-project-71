26b1d6a016456c098c53e6b5a13d9a82c1b1ff03
washburn <washburn@DESKTOP-PQRPMOP.localdomain>
2022-11-18 22:55:34 +0900

    step 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,26 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
     public static void main(String[] args) {
-        System.out.println("Hello World! for gradle");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("Compares two configuration files and shows a difference.");
+        return 0;
     }
 }
